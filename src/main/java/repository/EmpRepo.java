e2ff3e99dbcaae5354db89d0823c3112d5c7cd53
Akzhol <73769876+AkzholS7@users.noreply.github.com>
2022-04-10 17:23:06 +0600

    Add files via upload

---
+++ src/main/java/repository/EmpRepo.java
@@ -0,0 +1,11 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import entity.Employee;
+
+@Repository
+public interface EmpRepo  extends JpaRepository<Employee, Integer>{
+
+}
